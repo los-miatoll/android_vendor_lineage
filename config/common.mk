71e65d101b71457b949f856d0eb25e41d14e33a9
SwapnilVicky <swapnil.motoe@gmail.com>
2023-02-14 16:33:18 +0530

    vendor: Dont build Updater
    Useless as we dont ship OTAs
    
    Signed-off-by: Swapnil Vicky <swapnil.motoe@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -113,8 +113,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageParts \
     LineageSettingsProvider \
-    LineageSetupWizard \
-    Updater
+    LineageSetupWizard 
 
 # Themes
 PRODUCT_PACKAGES += \
